d543e2519923fe90eea9737865528f76959c0426
John McNeill <jh_mcneill@yahoo.ca>
2019-09-18 17:08:57 -0400

    Updates

---
+++ precache-manifest.44b67b7623b1fa09cbea6d4999154589.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a6e38040d00cc55595aa61a1bd92dd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e31db2d31c1d8aac9db0",
+    "url": "/monsters-rolodex/static/css/main.897aa686.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "e31db2d31c1d8aac9db0",
+    "url": "/monsters-rolodex/static/js/main.a59eb3dc.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
